776cb872999ad479fe8d6b8ddbc094fcc806279f
Mattazz <matthewazada101@gmail.com>
2025-01-23 08:29:59 -0500

    removed unused imports

--- src/main/java/com/discussionboard/db/DiscussionController.java
+++ src/main/java/com/discussionboard/db/DiscussionController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +12,6 @@ import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
